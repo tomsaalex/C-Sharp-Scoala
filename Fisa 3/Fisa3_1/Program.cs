1ae6ff097c355b680daa03a9f08fda801004998c
tomsaalex <tomsaalex@gmail.com>
2020-10-20 23:06:54 +0300

    Fisa3_1 && Fisa3_2
    Fisa3_1 && Fisa3_2

---
+++ Fisa 3/Fisa3_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Fisa3_1
+{
+    class Program
+    {
+        static bool Prim(int x)
+        {
+            if (x < 2)
+                return false;
+            if (x % 2 == 0 && x != 2)
+                return false;
+
+            for (int d = 3; d * d <= x; d += 2)
+                if (x % d == 0)
+                    return false;
+            
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Introdu numarul de elemente al sirului: ");
+            n = int.Parse(Console.ReadLine());
+            int[] v = new int[n];
+            int i = 0;
+            Console.Write("Introdu elementele sirului: ");
+            string s = Console.ReadLine();
+            string[] elements = s.Split(' ');
+
+            foreach(string e in elements)
+                v[++i] = int.Parse(e);
+
+            Console.Write("Elementele prime ale sirului sunt: ");
+
+            foreach(int element in v)
+            {
+                if (Prim(element))
+                    Console.Write($"{element} ");
+            }
+        }
+    }
+}
---
+++ Fisa 3/Fisa3_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Fisa3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, max = int.MinValue;
+            for(int i = 1; i <= 4; i++)
+            {
+                a = int.Parse(Console.ReadLine());
+                if (a > max)
+                    max = a;
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
