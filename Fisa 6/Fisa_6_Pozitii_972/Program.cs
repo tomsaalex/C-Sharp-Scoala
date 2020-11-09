0c2ea3d705a8be14c8a2a2c7bc92c0f2b00f00dd
tomsaalex <tomsaalex@gmail.com>
2020-11-09 23:50:40 +0200

    Fisa 5 si 6
    Fisa 5, actualizari, fisa 6, inceput.

---
+++ Fisa 6/Fisa_6_Pozitii_972/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Fisa_6_Pozitii_972
+{
+    class Program
+    {
+        public static bool IsVowel(char c)
+        {
+            string vowels = "aeiou";
+            if (vowels.Contains(c))
+                return true;
+            return false;
+        }
+
+        public static bool IsConsonant(char c)
+        {
+            if (Char.IsLetter(c) && !IsVowel(c))
+                return true;
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            char[] characters = s.ToCharArray();
+
+            int i;
+            int count = 0;
+            for(i = 0; i < characters.Length; i++)
+            {
+                if (i > 0 && IsConsonant(characters[i - 1]) && i < (characters.Length - 1) && IsConsonant(characters[i + 1]) && IsVowel(characters[i]))
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
